8841f07e1833915333a036617223e990c8d85a3c
Nguyen Thai Binh <admin@thaibinh.me>
2022-01-19 19:30:26 +0800

    isolate lexer and its components in its own namespace

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -5,43 +5,47 @@
 #ifndef FIRESTORM_LEXER_HPP
 #define FIRESTORM_LEXER_HPP
 
+#include "custom_exceptions.hpp"
+
 #include <fmt/format.h>
 #include <regex>
 #include <string>
 #include <utility>
 #include <vector>
 
-#include "utility.hpp"
-
-enum class Type {
-    Eof,
-    If,
-    Then,
-    Else,
+namespace Firestorm::Lexing {
+    /// @brief Represents different types of token in Firestorm code.
+    enum class Type {
+        Eof,
+        If,
+        Then,
+        Else,
 //    While,
-    Define,
-    Extern,
-    Number,
-    Plus,
-    Minus,
-    Times,
-    Divide,
-    Equ,
-    Lt,
+        Define,
+        Extern,
+        Number,
+        Plus,
+        Minus,
+        Times,
+        Divide,
+        Equ,
+        Lt,
 //    Equals,
-    Lparen,
-    Rparen,
-    Comma,
-    Semicolon,
-    Id,
-};
+        Lparen,
+        Rparen,
+        Comma,
+        Semicolon,
+        Id,
+    };
 
-namespace {
+    /// @brief Stores source position of the first character in each token.
     struct SourcePosition {
         long index = -1, lineno = -1, colno = -1;
     };
 
-    std::vector<std::pair<Type, std::regex>> &getRuleSet() {
+    /// Creates lexing rule sets for Firestorm's lexer.
+    /// @return The regular expression ruleset for Firestorm
+    static const std::vector<std::pair<Type, std::regex>> &getRuleSet() {
         static std::vector<std::pair<Type, std::regex>> rule_set;
 
         // Avoid multiple creation of rule set
@@ -92,50 +96,53 @@ namespace {
 
         return rule_set;
     }
+
+    /// @brief Consists of a single token extracted from source.
+    struct Token {
+        Type type{Type::Eof};
+        std::string value;
+        SourcePosition position{};
+
+        /// @return String representation of Token
+        [[nodiscard]]
+        std::string toString() const;
+
+        /// @return String representation of token's type
+        [[nodiscard]]
+        std::string getType() const;
+    };
+
+    struct Lexer;
+
+    /// @brief Can be used to retrieve tokens one-by-one from source;
+    struct TokenStream {
+        const Lexer &lexer;
+        const std::string& source;
+        long index = 0, lineno = 1, colno = 1;
+        Token currentToken;
+
+        TokenStream(const Lexer &l, const std::string& s) : lexer(l), source(s) { updateSourcePos(); }
+
+        /// @return Next token in source
+        Token getNextToken();
+
+    private:
+        /// @brief Updates index, line number and column number to the next character in source.
+        void updateSourcePos();
+    };
+
+    /// @brief Represents the rule set of Firestorm and acts as a wrapper of TokenStream
+    struct Lexer {
+        const std::vector<std::pair<Type, std::regex>> &rules;
+
+        Lexer() : rules(getRuleSet()) {}
+
+        /// \param input Firestorm source to be parsed
+        /// \return An instance of TokenStream
+        [[nodiscard]]
+        inline TokenStream lex(const std::string &input) const { return {*this, input}; }
+    };
 }
 
-struct Token {
-    Type type{Type::Eof};
-    std::string value;
-    SourcePosition position;
-
-    [[maybe_unused]] [[nodiscard]]
-    std::string toString() const;
-
-    [[nodiscard]]
-    inline bool isEOF() const { return type == Type::Eof; };
-
-    [[nodiscard]]
-    std::string getType() const;
-};
-
-struct Lexer;
-
-struct TokenStream {
-    const Lexer &lexer;
-    const std::string source;
-    long index = 0, lineno = 1, colno = 1;
-    Token currentToken;
-
-    TokenStream(const Lexer &l, std::string s) : lexer(l), source(std::move(s)) { updateSourcePos(); }
-
-    Token getNextToken();
-
-private:
-    void updateSourcePos();
-};
-
-struct Lexer {
-    const std::vector<std::pair<Type, std::regex>> &rules;
-
-    Lexer() : rules(getRuleSet()) {}
-
-    [[nodiscard]]
-    inline TokenStream lex(const std::string &input) const { return {*this, input}; }
-};
-
-struct LexingError : public FirestormError {
-    explicit LexingError(const char *msg) : FirestormError(msg) {}
-};
 
 #endif //FIRESTORM_LEXER_HPP
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,120 +1,123 @@
 //
 // Created by Nguyen Thai Binh on 16/1/22.
 //
+#include "custom_exceptions.hpp"
+#include "lexer.hpp"
+
 #include <algorithm>
 #include <fmt/format.h>
 #include <map>
 #include <regex>
 
-#include "lexer.hpp"
+namespace Firestorm::Lexing {
+    [[maybe_unused]]
+    std::string Token::toString() const {
+        std::string msg = "Token(type={}, value={}, index={}, lineno={}, colno={})";
+        return fmt::format(msg, getType(), value, position.index, position.lineno, position.colno);
+    }
 
-[[maybe_unused]]
-std::string Token::toString() const {
-    std::string msg = "Token(type={}, value={}, index={}, lineno={}, colno={})";
-    return fmt::format(msg, getType(), value, position.index, position.lineno, position.colno);
-}
-
-const auto &getTypeName(Type type) {
-    static std::map<Type, std::string> type_map{
-            {Type::Eof, "EOF"},
-            {Type::If, "IF"},
-            {Type::Then, "THEN"},
-            {Type::Else, "ELSE"},
+    const auto &getTypeName(Type type) {
+        static std::map<Type, std::string> type_map{
+                {Type::Eof,    "EOF"},
+                {Type::If,     "IF"},
+                {Type::Then,   "THEN"},
+                {Type::Else,   "ELSE"},
 //            {Type::While, "WHILE"},
-            {Type::Define, "DEFINE"},
-            {Type::Extern, "EXTERN"},
-            {Type::Number, "NUMBER"},
-            {Type::Plus, "PLUS"},
-            {Type::Minus, "MINUS"},
-            {Type::Times, "TIMES"},
-            {Type::Divide, "DIVIDE"},
-            {Type::Equ, "EQU"},
-            {Type::Lt, "LT"},
+                {Type::Define, "DEFINE"},
+                {Type::Extern, "EXTERN"},
+                {Type::Number, "NUMBER"},
+                {Type::Plus,   "PLUS"},
+                {Type::Minus,  "MINUS"},
+                {Type::Times,  "TIMES"},
+                {Type::Divide, "DIVIDE"},
+                {Type::Equ,    "EQU"},
+                {Type::Lt,     "LT"},
 //            {Type::Equals, "EQUALS"},
-            {Type::Lparen, "LPAREN"},
-            {Type::Rparen, "RPAREN"},
-            {Type::Comma, "COMMA"},
-            {Type::Id, "ID"},
-    };
-    return type_map[type];
-}
+                {Type::Lparen, "LPAREN"},
+                {Type::Rparen, "RPAREN"},
+                {Type::Comma,  "COMMA"},
+                {Type::Id,     "ID"},
+        };
+        return type_map[type];
+    }
 
-std::string Token::getType() const {
-    return getTypeName(type);
-}
+    std::string Token::getType() const {
+        return getTypeName(type);
+    }
 
-Token TokenStream::getNextToken() {
-    // Check if finished, return EOF token
-    if (index == source.length()) return currentToken = {Type::Eof, "EOF", {index, lineno, colno}};
+    Token TokenStream::getNextToken() {
+        // Check if finished, return EOF token
+        if (index == source.length()) return currentToken = {Type::Eof, "EOF", {index, lineno, colno}};
 
-    // Get substring, ignoring already lexed
-    auto sub_str = source.substr(index, source.length() - index);
+        // Get substring, ignoring already lexed
+        auto sub_str = source.substr(index, source.length() - index);
 
-    // Iterate over all rules
-    std::smatch match_info;
-    for (const auto &rule: lexer.rules) {
-        // Search substring against rule
-        if (std::regex_search(sub_str, match_info, rule.second)) {
-            // Get the matched string
-            auto matched = sub_str.substr(match_info.position(), match_info.length());
+        // Iterate over all rules
+        std::smatch match_info;
+        for (const auto &rule: lexer.rules) {
+            // Search substring against rule
+            if (std::regex_search(sub_str, match_info, rule.second)) {
+                // Get the matched string
+                auto matched = sub_str.substr(match_info.position(), match_info.length());
 
-            // Craft token
-            currentToken = {rule.first, matched, {index, lineno, colno}};
+                // Craft token
+                currentToken = {rule.first, matched, {index, lineno, colno}};
 
-            // Update position
-            updateSourcePos();
+                // Update position
+                updateSourcePos();
 
-            return currentToken;
+                return currentToken;
+            }
         }
-    }
-    // Throw an error when source doesn't match any rules
-    throw LexingError(fmt::format("Unexpected character at line {}, column {}", lineno, colno).c_str());
-}
-
-void TokenStream::updateSourcePos() {
-    // Increment index and colno by the length of the token
-    auto l = (long) currentToken.value.length();
-    index += l;
-    colno += l;
-
-    // Find the first character (from index) that is not a whitespace
-    auto first_non_ws = source.find_first_not_of(" \n", index);
-
-    // If there aren't any whitespaces, return
-    if (first_non_ws == index) return;
-
-    // Check if end of source, i.e. the rest are whitespaces
-    if (first_non_ws == std::string::npos) {
-        // Set line, column and index
-        index = (long) source.length();
-        lineno = std::count(source.begin(), source.end(), '\n') + 1;
-        colno = (long) (source.length() - source.rfind('\n') + 1);
-        return;
+        // Throw an error when source doesn't match any rules
+        throw Utility::getError("[{}:{}] Unknown character '{}'", lineno, colno, sub_str[0]);
     }
 
-    // Substring to match against (from index to first non-whitespace)
-    auto sub_str = source.substr(index, first_non_ws - index);
+    void TokenStream::updateSourcePos() {
+        // Increment index and colno by the length of the token
+        auto l = (long) currentToken.value.length();
+        index += l;
+        colno += l;
 
-    // Increment index by length of whitespaces
-    index += (long) sub_str.length();
+        // Find the first character (from index) that is not a whitespace
+        auto first_non_ws = source.find_first_not_of(" \n", index);
 
-    // Check if there are newlines in substring
-    if (sub_str.find('\n') != std::string::npos) {
-        // Count the number of newline(s)
-        auto nl_count = std::count(sub_str.begin(), sub_str.end(), '\n');
+        // If there aren't any whitespaces, return
+        if (first_non_ws == index) return;
 
-        // Increment line number by the number of newline(s)
-        lineno += nl_count;
+        // Check if end of source, i.e. the rest are whitespaces
+        if (first_non_ws == std::string::npos) {
+            // Set line, column and index
+            index = (long) source.length();
+            lineno = std::count(source.begin(), source.end(), '\n') + 1;
+            colno = (long) (source.length() - source.rfind('\n') + 1);
+            return;
+        }
 
-        // Find last newline in substring
-        auto last_nl = sub_str.rfind('\n');
+        // Substring to match against (from index to first non-whitespace)
+        auto sub_str = source.substr(index, first_non_ws - index);
 
-        // Set column number to the number of whitespaces after the last newline
-        colno = (long) (sub_str.length() - last_nl);
-        return;
+        // Increment index by length of whitespaces
+        index += (long) sub_str.length();
+
+        // Check if there are newlines in substring
+        if (sub_str.find('\n') != std::string::npos) {
+            // Count the number of newline(s)
+            auto nl_count = std::count(sub_str.begin(), sub_str.end(), '\n');
+
+            // Increment line number by the number of newline(s)
+            lineno += nl_count;
+
+            // Find last newline in substring
+            auto last_nl = sub_str.rfind('\n');
+
+            // Set column number to the number of whitespaces after the last newline
+            colno = (long) (sub_str.length() - last_nl);
+            return;
+        }
+
+        // Otherwise, if there aren't any newlines, update
+        // column according to length of substring
+        colno += (long) sub_str.length();
     }
-
-    // Otherwise, if there aren't any newlines, update
-    // column according to length of substring
-    colno += (long) sub_str.length();
 }
